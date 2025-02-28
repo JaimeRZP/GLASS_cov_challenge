78b4b0483de88a7b79454d47e0866f1ab81b21f9
davidesciotti <84071067+davidesciotti@users.noreply.github.com>
2025-02-28 11:35:09 +0100

    fmt + pos/shear nz split

--- scripts/sb_interface.py
+++ scripts/sb_interface.py
@@ -1,25 +1,29 @@
+from matplotlib import pyplot as plt
 import numpy as np
 import heracles
 import yaml
 import subprocess
-import os
 
 
 def write_cl_tab(ascii_folder, ascii_filename, cl_3d, ells, zbins):
-
     with open(f'{ascii_folder}/{ascii_filename}', 'w') as file:
         file.write(f'#ell\t\tzi\tzj\t{ascii_filename}\n')
         for ell_idx, ell_val in enumerate(ells):
             for zi in range(zbins):
                 for zj in range(zbins):
                     value = cl_3d[ell_idx, zi, zj]
-                    file.write(f"{ell_val:.3f}\t\t{zi}\t{zj}\t{value:.10e}\n")
+                    file.write(f'{ell_val:.3f}\t\t{zi}\t{zj}\t{value:.10e}\n')
 
 
 # ===== SETTINGS ========
 path_to_sb_main = '/home/davide/Documenti/Lavoro/Programmi/Spaceborne/main.py'
-path_to_sb_cfg = '/home/davide/Documenti/Lavoro/Programmi/GLASS_cov_challenge/config_spaceborne_out.yaml'
+path_to_sb_cfg = (
+    '/home/davide/Documenti/Lavoro/Programmi/GLASS_cov_challenge/'
+    'config_spaceborne_out.yaml'
+)
 data_sb_path = '../data/data_sb'
+nz_path = '../data'
+
 Nbins = 2
 ls = np.arange(256 + 1)
 
@@ -27,13 +31,13 @@ with open('../config_spaceborne_in.yaml', 'r') as f:
     cfg = yaml.safe_load(f)
 
 # TODO Jaime: there are actually important, check values used for the challenge
-cfg['nz']['ngal_sources'] = [10., 10.]  # gal/arcmin^2/zbin
-cfg['nz']['ngal_lenses'] = [10., 10.]  # gal/arcmin^2/zbin
-cfg['covariance']['sigma_eps_i'] = 0.
+cfg['nz']['ngal_sources'] = [10.0, 10.0]  # gal/arcmin^2/zbin
+cfg['nz']['ngal_lenses'] = [10.0, 10.0]  # gal/arcmin^2/zbin
+cfg['covariance']['sigma_eps_i'] = 0.0
 # ===== END SETTINGS ========
 
-cfg['nz']['dzGC'] = [0.] * Nbins
-cfg['nz']['dzWL'] = [0.] * Nbins
+cfg['nz']['dzGC'] = [0.0] * Nbins
+cfg['nz']['dzWL'] = [0.0] * Nbins
 cfg['misc']['output_path'] = data_sb_path
 
 # ell binning
@@ -54,33 +58,40 @@ cfg['C_ell']['mult_shear_bias'] = [0] * Nbins
 cfg['C_ell']['mult_shear_bias'] = [0] * Nbins
 
 # n(z) - THIS IS USED ONLY FOR THE NON-GAUSSIAN COVARIANCE
-z_nz = np.load('/home/davide/Documenti/Lavoro/Programmi/GLASS_cov_challenge/data/nzs.npz')['z']
-nz_1 = np.load('/home/davide/Documenti/Lavoro/Programmi/GLASS_cov_challenge/data/nzs.npz')['nz_1']
-nz_2 = np.load('/home/davide/Documenti/Lavoro/Programmi/GLASS_cov_challenge/data/nzs.npz')['nz_2']
-# plt.plot(z_nz, nz_1[0, :], label='nz_1[0]')
-# plt.plot(z_nz, nz_1[1, :], label='nz_1[1]')
-# plt.plot(z_nz, nz_2[0, :], label='nz_2[0]')
-# plt.plot(z_nz, nz_2[1, :], label='nz_2[1]')
-nz_sb_fmt = np.concatenate((z_nz[:, None], nz_1.T), axis=1)
-np.savetxt(f'{data_sb_path}/nz_sb_fmt.txt', nz_sb_fmt)
-cfg['nz']['nz_lenses_filename'] = f'{data_sb_path}/nz_sb_fmt.txt'
-cfg['nz']['nz_sources_filename'] = f'{data_sb_path}/nz_sb_fmt.txt'
+z_nz = np.load(f'{nz_path}/nzs.npz')['z']
+nz_pos = np.load(f'{nz_path}/nzs.npz')['nz_1']
+nz_she = np.load(f'{nz_path}/nzs.npz')['nz_2']
+
+plt.plot(z_nz, nz_pos[0, :], label='nz_pos[0]')
+plt.plot(z_nz, nz_pos[1, :], label='nz_pos[1]')
+plt.plot(z_nz, nz_she[0, :], label='nz_she[0]')
+plt.plot(z_nz, nz_she[1, :], label='nz_she[1]')
+plt.legend()
+
+nz_pos_fmt = np.concatenate((z_nz[:, None], nz_pos.T), axis=1)
+nz_she_fmt = np.concatenate((z_nz[:, None], nz_she.T), axis=1)
+np.savetxt(f'{data_sb_path}/nz_pos_fmt.txt', nz_pos_fmt)
+np.savetxt(f'{data_sb_path}/nz_she_fmt.txt', nz_she_fmt)
+cfg['nz']['nz_lenses_filename'] = f'{data_sb_path}/nz_pos_fmt.txt'
+cfg['nz']['nz_sources_filename'] = f'{data_sb_path}/nz_she_fmt.txt'
 
 cfg['covariance']['split_gaussian_cov'] = False
+cfg['covariance']['SSC'] = False
+cfg['covariance']['cNG'] = False
 
 # ================== HERACLES Cls ==================
-theory_cls = heracles.read("../data/theory_cls.fits")
+theory_cls = heracles.read('../data/theory_cls.fits')
 _theory_cls = {}
-_theory_cls[("POS", "POS", 1, 1)] = theory_cls["W1xW1"]
-_theory_cls[("POS", "POS", 2, 2)] = theory_cls["W2xW2"]
-_theory_cls[("G_E", "G_E", 1, 1)] = theory_cls["W3xW3"]
-_theory_cls[("G_E", "G_E", 2, 2)] = theory_cls["W4xW4"]
-_theory_cls[("POS", "G_E", 1, 1)] = -theory_cls["W1xW3"].__array__()
-_theory_cls[("POS", "G_E", 2, 2)] = -theory_cls["W2xW4"].__array__()
-_theory_cls[("POS", "POS", 1, 2)] = theory_cls["W1xW2"]
-_theory_cls[("POS", "G_E", 1, 2)] = -theory_cls["W1xW4"].__array__()
-_theory_cls[("POS", "G_E", 2, 1)] = -theory_cls["W2xW3"].__array__()
-_theory_cls[("G_E", "G_E", 1, 2)] = theory_cls["W3xW4"]
+_theory_cls[('POS', 'POS', 1, 1)] = theory_cls['W1xW1']
+_theory_cls[('POS', 'POS', 2, 2)] = theory_cls['W2xW2']
+_theory_cls[('G_E', 'G_E', 1, 1)] = theory_cls['W3xW3']
+_theory_cls[('G_E', 'G_E', 2, 2)] = theory_cls['W4xW4']
+_theory_cls[('POS', 'G_E', 1, 1)] = -theory_cls['W1xW3'].__array__()
+_theory_cls[('POS', 'G_E', 2, 2)] = -theory_cls['W2xW4'].__array__()
+_theory_cls[('POS', 'POS', 1, 2)] = theory_cls['W1xW2']
+_theory_cls[('POS', 'G_E', 1, 2)] = -theory_cls['W1xW4'].__array__()
+_theory_cls[('POS', 'G_E', 2, 1)] = -theory_cls['W2xW3'].__array__()
+_theory_cls[('G_E', 'G_E', 1, 2)] = theory_cls['W3xW4']
 
 
 __theory_gg = np.zeros((len(ls), Nbins, Nbins))
@@ -88,14 +99,14 @@ __theory_gs = np.zeros((len(ls), Nbins, Nbins))
 __theory_ss = np.zeros((len(ls), Nbins, Nbins))
 for i in range(1, Nbins + 1):
     for j in range(i, Nbins + 1):
-        __theory_gg[:, i - 1, j - 1] = _theory_cls[("POS", "POS", i, j)]
-        __theory_gs[:, j - 1, i - 1] = _theory_cls[("POS", "POS", i, j)]
+        __theory_gg[:, i - 1, j - 1] = _theory_cls[('POS', 'POS', i, j)]
+        __theory_gs[:, j - 1, i - 1] = _theory_cls[('POS', 'POS', i, j)]
 
-        __theory_gs[:, i - 1, j - 1] = _theory_cls[("POS", "G_E", i, j)]
-        __theory_gs[:, j - 1, i - 1] = _theory_cls[("POS", "G_E", i, j)]
+        __theory_gs[:, i - 1, j - 1] = _theory_cls[('POS', 'G_E', i, j)]
+        __theory_gs[:, j - 1, i - 1] = _theory_cls[('POS', 'G_E', i, j)]
 
-        __theory_ss[:, i - 1, j - 1] = _theory_cls[("G_E", "G_E", i, j)]
-        __theory_ss[:, j - 1, i - 1] = _theory_cls[("G_E", "G_E", i, j)]
+        __theory_ss[:, i - 1, j - 1] = _theory_cls[('G_E', 'G_E', i, j)]
+        __theory_ss[:, j - 1, i - 1] = _theory_cls[('G_E', 'G_E', i, j)]
 
 write_cl_tab(data_sb_path, 'cl_ll_tab.txt', __theory_ss, ls, Nbins)
 write_cl_tab(data_sb_path, 'cl_gl_tab.txt', __theory_gs, ls, Nbins)
@@ -135,6 +146,27 @@ for probe_a_ix in range(2):
                                 probe_b_str = probe_dict[probe_b_ix]
                                 probe_c_str = probe_dict[probe_c_ix]
                                 probe_d_str = probe_dict[probe_d_ix]
-                                cov_dict[(probe_a_str, probe_b_str, probe_c_str, probe_d_str, zi, zj, zk, zl)] = \
-                                    cov_10d[probe_a_ix, probe_b_ix, probe_c_ix, probe_d_ix, :, :, zi, zj, zk, zl]
 
+                                cov_dict[
+                                    (
+                                        probe_a_str,
+                                        probe_b_str,
+                                        probe_c_str,
+                                        probe_d_str,
+                                        zi,
+                                        zj,
+                                        zk,
+                                        zl,
+                                    )
+                                ] = cov_10d[
+                                    probe_a_ix,
+                                    probe_b_ix,
+                                    probe_c_ix,
+                                    probe_d_ix,
+                                    :,
+                                    :,
+                                    zi,
+                                    zj,
+                                    zk,
+                                    zl,
+                                ]
