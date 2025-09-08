7c370f29cf9ac33aa376cf351ecfb401cf50d9ac
jaimerzp <jaimerz011235813@gmail.com>
2025-09-08 16:09:26 +0100

    kernels

---
+++ scripts/namaster.py
@@ -0,0 +1,73 @@
+import yaml
+import numpy as np
+import healpy as hp
+import heracles
+import pymaster as nmt
+
+# conda activate nmt
+def compute_master(f_a, f_b, wsp):
+    cl_coupled = nmt.compute_coupled_cell(f_a, f_b)
+    cl_decoupled = wsp.decouple_cell(cl_coupled)
+    return cl_decoupled
+
+# Config
+config_path = "./sims_config.yaml"
+with open(config_path, 'r') as f:
+    config = yaml.safe_load(f)
+n = config['nsims']
+nside = config['nside']
+lmax = config['lmax']
+nlbins = config.get('nlbins', 20)  # Default to 20 if not specified
+mode = config['mode']  # "lognormal" or "gaussian"
+mask_type = config['mask_type']  # Default to 'Patch' if not specified
+path = f"../{mask_type}"
+
+ls = np.arange(lmax + 1)
+ledges = np.logspace(np.log10(10), np.log10(lmax), nlbins + 1)
+lgrid = ledges[:-1] + np.diff(ledges)/2
+b = nmt.NmtBin.from_edges(ledges[:-1].astype(int), ledges[1:].astype(int))
+b.lmax = lmax
+
+mask = hp.read_map(f"{path}/mask.fits")
+msk_apo = nmt.mask_apodization(mask, 10.0, apotype='C1')
+
+# workspaces
+sim_path = f"../{mode}_sims/{mode}_sim_1"
+POS1 = heracles.read_maps(f"{sim_path}/POS_1.fits")
+SHE1 = heracles.read_maps(f"{sim_path}/SHE_1.fits")
+map_t = POS1['POS', 1]
+map_q = SHE1['SHE', 1][0]
+map_u = SHE1['SHE', 1][1]
+f0 = nmt.NmtField(msk_apo, [map_t], lmax=lmax)
+f2 = nmt.NmtField(msk_apo, [map_q, map_u], lmax=lmax)
+w00 = nmt.NmtWorkspace.from_fields(f0, f0, b)
+w02 = nmt.NmtWorkspace.from_fields(f0, f2, b)
+w22 = nmt.NmtWorkspace.from_fields(f2, f2, b)
+
+for i in range(1, n+1):
+    print(f"Unmixing sim {i}", end='\r')
+    # Load maps
+    sim_path = f"../{mode}_sims/{mode}_sim_{i}"
+    POS1 = heracles.read_maps(f"{sim_path}/POS_1.fits")
+    SHE1 = heracles.read_maps(f"{sim_path}/SHE_1.fits")
+    map_t = POS1['POS', 1]
+    map_q = SHE1['SHE', 1][0]
+    map_u = SHE1['SHE', 1][1]
+    # Make fields
+    f0 = nmt.NmtField(msk_apo, [map_t])
+    f2 = nmt.NmtField(msk_apo, [map_q, map_u])
+    # Compute cls
+    cls_00 = compute_master(f0, f0, w00)
+    cls_02 = compute_master(f0, f2, w02)
+    cls_22 = compute_master(f2, f2, w22)
+    # reorder
+    _cls_nmt= {}
+    #_cls_yp = {}
+    _cls_nmt['POS', 'POS', 1, 1] = heracles.Result(cls_00[0], axis=(0,), ell=lgrid)
+    _cls_nmt['POS', 'SHE', 1, 1] = heracles.Result(cls_02, axis=(1,), ell=lgrid)
+    _cls_nmt['SHE', 'SHE', 1, 1] = heracles.Result(
+        np.array([[cls_22[0], cls_22[1]],
+                 [cls_22[2], cls_22[3]]]),
+        axis=(2,), ell=lgrid)
+    # Save results
+    heracles.write(f"{path}/cls_nmt/cqs_data_nmt_np_{i}.fits", _cls_nmt)
